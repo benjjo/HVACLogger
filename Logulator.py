df70ccc4aba5525d80da1b31a8ea770ad220ea5c
benjjo <benjjo@gmail.com>
2021-02-01 11:59:35 +0000

    Data Logger data can be processed. However, a little clunky atm. Tested OK but needs work.

--- Logulator.py
+++ Logulator.py
@@ -12,20 +12,19 @@ class Logulator:
 
     def getAllData(self):
         if 'all_data.csv' not in os.listdir(self.path):
-            self.setupCSVFiles()
-            self.writeTempCSVfiles()
+            self.setupCSVFiles('xls')
+            self.writeTempCSVFiles()
             self.csvToDataFrame()
             self.writeAllDataCSV()
             self.all_data.to_csv("all_data.csv", index=False)
 
         return pd.read_csv('all_data.csv')
 
-    def setupCSVFiles(self):
+    def setupCSVFiles(self, extension):
         self.path = os.getcwd()
         files = os.listdir(self.path)
         os.makedirs(self.tempDir, exist_ok=True)
-        files_xls = [f for f in files if f[-3:] == 'xls']
-
+        files_xls = [f for f in files if f[-3:] == extension]
         counter = 0
         for file in files_xls:
             with open(file) as fin, open(self.tempDir + str(counter) + '_New_File.txt', 'w') as fout:
@@ -33,11 +32,9 @@ class Logulator:
                     fout.write(line.replace('\t', ','))
                 counter += 1
 
-    def writeTempCSVfiles(self):
+    def writeTempCSVFiles(self):
         tempFiles = os.listdir(self.tempDir)
-
         files_txt = [f for f in tempFiles if f[-3:] == 'txt']
-
         counter = 0
         for file in files_txt:
             with open(self.tempDir + file) as fin, open(self.tempDir + str(counter) + '_final.csv', 'w') as fout:
@@ -45,6 +42,26 @@ class Logulator:
                     fout.write(line.replace('BERTH_9_FEEDBACK,BERTH_10_FEEDBACK,',
                                             'BERTH_9_FEEDBACK,BERTH_10_FEEDBACK,NothinToSeeHere,'))
                 counter += 1
+
+    def writeTempLoggerFiles(self):
+        tempLogs = pd.DataFrame()
+        self.path = os.getcwd()
+        files = os.listdir(self.path)
+        os.makedirs(self.tempDir, exist_ok=True)
+        files_xlsx = [f for f in files if f[-4:] == 'xlsx']
+        counter = 0
+        for file in files_xlsx:
+            os.rename(file, self.tempDir + str(counter) + '_New_File.xlsx')
+            data = pd.read_excel(self.tempDir + str(counter) + '_New_File.xlsx', engine='openpyxl')
+            tempLogs = tempLogs.append(data)
+            counter += 1
+        tempLogs.to_csv('loggerData.csv')
+
+
+    def getDataLoggerDF(self):
+        if 'loggerData.csv' not in os.listdir(self.path):
+            self.writeTempLoggerFiles()
+        return pd.read_csv('loggerData.csv')
 
     def writeAllDataCSV(self):
         self.all_data.to_csv("all_data.csv", index=False)
@@ -93,6 +110,18 @@ class Logulator:
         damperData = damperData.set_index('Time date')
         return damperData
 
+    def getDataLogger(self):
+        df = self.getDataLoggerDF()
+        df.head()
+        dataLogger = pd.DataFrame()
+        dataLogger['Time date'] = df['Time']
+        dataLogger['Temperature °C'] = df['Celsius(°C)']
+        dataLogger.to_csv("dataLogger.csv", index=False)
+        dataLogger = pd.read_csv("dataLogger.csv")
+
+        dataLogger = dataLogger.set_index('Time date')
+        return dataLogger
+
 
 class TempLogger(Logulator):
     def plotTemperatures(self):
@@ -136,12 +165,34 @@ class DampLogger(Logulator):
         plt.show()
 
 
+class dataLoggerTemperatures(Logulator):
+    def plotDataLoggerTemps(self):
+
+        dfTemp = Logulator.getDataLogger(self).copy()
+        dfTemp.plot(kind='line', legend=None)
+        plt.xticks(color='C0', rotation='vertical')
+        plt.xlabel('Time date', color='C0', size=10)
+        plt.yticks(color='C0')
+        plt.tight_layout(pad=2)
+        plt.title('HVAC Temperatures', color='C0')
+        plt.ylabel('Temperature', color='C0', size=10)
+        plt.grid('on', linestyle='--')
+        lgd = plt.legend(title='Channel', bbox_to_anchor=(1.05, 1))
+
+        plt.savefig('myfig100.png', dpi=300, facecolor='w', edgecolor='w',
+                    orientation='landscape', format=None, bbox_extra_artists=(lgd,), bbox_inches='tight',
+                    transparent=False, pad_inches=0.1)
+        plt.show()
+
+
 def main():
     temp = TempLogger()
     damp = DampLogger()
+    dataLog = dataLoggerTemperatures()
 
-    temp.plotTemperatures()
-    damp.plotDamperPositions()
+    # temp.plotTemperatures()
+    # damp.plotDamperPositions()
+    dataLog.plotDataLoggerTemps()
 
 
 if __name__ == "__main__":
